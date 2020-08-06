4023ee2baa3e6135eee98014edf04f945a37e162
ccrossman <christopher.j.crossman@gmail.com>
2020-08-05 17:27:33 -0700

    Added a user submission page.

--- src/main/java/com/crossman/User.java
+++ src/main/java/com/crossman/User.java
@@ -2,6 +2,7 @@ package com.crossman;
 
 import lombok.Value;
 
+import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,6 +13,6 @@ public class User {
 	Set<Address> addresses;
 
 	public StoredUser toStoredUser() {
-		return new StoredUser(email, firstName, lastName, addresses.stream().map(Address::toStoredAddress).collect(Collectors.toSet()));
+		return new StoredUser(email, firstName, lastName, addresses == null ? Collections.emptySet() : addresses.stream().map(Address::toStoredAddress).collect(Collectors.toSet()));
 	}
 }
--- src/main/java/com/crossman/ViewController.java
+++ src/main/java/com/crossman/ViewController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collections;
 
 @Controller
 public class ViewController {
@@ -16,8 +19,24 @@ public class ViewController {
 
 	@RequestMapping("/users")
 	public String users(Model model) {
+		logger.debug("users()");
 		Iterable<StoredUser> users = apiController.findAllUsers();
 		model.addAttribute("users", users);
 		return "users";
+	}
+
+	@RequestMapping("/user")
+	public String userCreator(Model model) {
+		logger.debug("userCreator()");
+		model.addAttribute("user", new User(null, null, null, Collections.emptySet()));
+		return "userCreator";
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.POST, consumes = {"application/x-www-form-urlencoded;charset=UTF-8"})
+	public String userCreatorSubmit(Model model, User user) {
+		logger.debug("userCreatorSubmit({})", user);
+		StoredUser storedUser = apiController.createUser(user);
+		model.addAttribute("user", user);
+		return "userCreated";
 	}
 }
